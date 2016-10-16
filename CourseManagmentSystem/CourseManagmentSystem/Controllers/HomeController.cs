77b6462c32ff9e94e565ab4f40b4b997b50d26ba
AnnaZinovatnaya <zinovatnaya@gmail.com>
2016-10-16 20:41:55 +0300

    UserProfile and sceleton for course (v.2)

--- CourseManagmentSystem/CourseManagmentSystem/Controllers/HomeController.cs
+++ CourseManagmentSystem/CourseManagmentSystem/Controllers/HomeController.cs
@@ -4,15 +4,17 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
+using CourseManagmentSystem.Models;
 
 namespace CourseManagmentSystem.Controllers
 {
     public class HomeController : Controller
     {
+        private AppDbContext db = new AppDbContext();
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            return View(db.Courses.ToList());
         }
 
     }
